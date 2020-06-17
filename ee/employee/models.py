80c0adb9f6a8433542b747adccb6049ec18614a7
afieqhamieza <afieqhaazemi@gmail.com>
2020-06-17 11:59:03 +0800

    put the employee list in a card

--- ee/employee/models.py
+++ ee/employee/models.py
@@ -5,6 +5,17 @@ from django.db import models
 class Employee(models.Model):
     name = models.CharField(max_length=255)
     staffId = models.IntegerField()
-    level = models.IntegerField()
+    level = models.CharField(max_length=255)
     team = models.CharField(max_length=255)
 
+# senior executive
+# executive
+# senior director
+# director
+# senior manager
+# manager
+# senior advisor
+# advisor
+# senior staffId
+# intermediate staff
+# associate staff
