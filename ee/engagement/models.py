b238767398a1b019952ed14ac8ae41d58ac93011
afieqhamieza <afieqhaazemi@gmail.com>
2020-07-06 10:52:22 +0800

    bugs fixed siket

--- ee/engagement/models.py
+++ ee/engagement/models.py
@@ -2,15 +2,22 @@ from django.db import models
 
 # Create your models here.
 
+# Create your models here.
+class Staff(models.Model):
+    name = models.TextField(null = True, blank = True)
+    staff_id = models.IntegerField(null = True, blank=True, default='0')
+
 class Engagement(models.Model):
-    title = models.CharField(max_length=255,null = True, blank = True)
+    title = models.CharField(max_length=255,null = True, blank = True) 
     body = models.TextField(null = True, blank = True)
     # add on
     startDate = models.DateField(null = True, blank = True)
     endDate = models.DateField(null = True, blank = True)
     status = models.CharField(max_length=30, null = True, blank = True, default='just started')
-    staff = models.TextField(null = True, blank = True)
+    staff = models.ManyToManyField(Staff)
     progress = models.IntegerField(null = True, blank=True, default='0')
+
+
 
     
 
--- ee/engagement/views.py
+++ ee/engagement/views.py
@@ -12,21 +12,39 @@ from django.contrib import messages
 
 #a snippet of information of each engagement
 def engagement_index(request):
-    engagement = Engagement.objects.all()   #the query
+    # import pdb; pdb.set_trace()
+    engagement_list = Engagement.objects.all()   #the query
 
     if (request.method == 'POST') and ("addProj" in request.POST):
         form = EngagementForm(request.POST) #creating instance of the form
+        # import pdb; pdb.set_trace()
 
         if form.is_valid():
+            # import pdb; pdb.set_trace()
             # save user's input to the database here
             form.save()
+
+
+
+
+            staff_input = request.POST.get('staff')
+            import pdb; pdb.set_trace()
+            #change the string in staff_input to fields of staff object
+            #declare an instance of class
+            form.staff.add(staff_input)
+ 
+
+
+
+
+
             messages.success(request, f'{"Form submission successful"}')
             return redirect('engagement_index')   #here should enter a new url where it will go if successful
     else: # means GET
         form = EngagementForm()
     
     context = {     #context dictionary
-        'engagements':  engagement,
+        'engagements':  engagement_list,
         'form': form,
     }
     return render(request, 'engagement_index.html', context)
