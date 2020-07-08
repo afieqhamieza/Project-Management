eb86d90162abe7c251a70b09e6f5aa0c66c91d17
afieqhamieza <afieqhaazemi@gmail.com>
2020-07-08 17:13:00 +0800

    TIMELINE DONE!!!!

--- ee/homepage/views.py
+++ ee/homepage/views.py
@@ -11,9 +11,16 @@ def homepage(request):
     employee_list = Employee.objects.all()
     employee_count = len(employee_list)
 
+    temp = list(reversed(engagement_list))
+    latest7 = temp[:7]
+
+    engagement_byEndDate = Engagement.objects.order_by('endDate', 'startDate') 
+
     context= {
         'engCount': engagement_count,
-        'empCount': engagement_count
+        'empCount': engagement_count,
+        'latest7': latest7,
+        'eng_byEndDate': engagement_byEndDate,
     }
 
     return render(request, 'homepage.html', context)
