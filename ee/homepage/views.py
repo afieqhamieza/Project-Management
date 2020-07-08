041cd2db573dd80a41e6e95913dc235db10f5fbd
afieqhamieza <afieqhaazemi@gmail.com>
2020-07-08 10:22:25 +0800

    timeline looks good

--- ee/homepage/views.py
+++ ee/homepage/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from engagement.models import Engagement
 
 # Create your views here.
 
 def homepage(request):
+
     return render(request, 'homepage.html', {})
