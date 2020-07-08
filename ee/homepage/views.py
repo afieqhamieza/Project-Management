b77f4d0d0ca6d17c6458c79738f299246b14748b
afieqhamieza <afieqhaazemi@gmail.com>
2020-07-08 11:06:41 +0800

    add on css but all still hardcoded

--- ee/homepage/views.py
+++ ee/homepage/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from engagement.models import Engagement
+from employee.models import Employee
 
 # Create your views here.
 
 def homepage(request):
+    engagement_list = Engagement.objects.all()
+    engagement_count = len(engagement_list)
 
-    return render(request, 'homepage.html', {})
+    employee_list = Employee.objects.all()
+    employee_count = len(employee_list)
+
+    context= {
+        'engCount': engagement_count,
+        'empCount': engagement_count
+    }
+
+    return render(request, 'homepage.html', context)
