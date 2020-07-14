7e324933c3742a65b4101c7d6e460581776294f8
afieqhamieza <afieqhaazemi@gmail.com>
2020-07-14 15:45:14 +0800

    minor fixes

--- ee/engagement/urls.py
+++ ee/engagement/urls.py
@@ -5,7 +5,5 @@ from django.conf.urls import url
 urlpatterns = [
     path("", views.engagement_index, name="engagement_index"),
     path("<int:pk>/", views.engagement_detail, name="engagement_detail"),
-    # url("/getengagementid/view/", name="getengagementid")
-    # url(r'^/getengagementid/view/', views.getengagementid, name='getengagementid'),
     path("editEngagement/", views.getengagementid, name='getengagementid'),
 ]
